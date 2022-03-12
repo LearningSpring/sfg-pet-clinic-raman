5eaa5b4398c5d4b2c9d65d529b30fd64f451448c
ramandeepsingh <ramandeep.singh.in@gmail.com>
2022-03-12 20:05:35 +0530

    maven realease plugin

---
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package sfgpetclinic;
+
+import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SfgPetClinicApplication.class)
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
